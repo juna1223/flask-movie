8a20a7f8bb46dcf304f1042ac8d7b4be4a2e74e9
juna1223 <junha12232@gmail.com>
2022-01-12 15:58:50 +0900

    first

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+# JWT 사용을 위한 SECRET_KEY 정보가 들어있는 파일 임포트
+from config import Config
+from flask.json import jsonify
+from http import HTTPStatus
+
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from resources.favorite import FavoriteListResource, FavoriteResource
+from resources.login import UserLoginResource
+from resources.movie import MovieListResource, MovieRealtimeRecommResource, MovieRecommResource, MovieSearchResource
+
+from resources.register import UserRegisterResource
+
+from resources.logout import UserLogoutResource, jwt_blacklist
+from resources.review import ReviewResource
+from resources.user import UserInfoResource
+
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config)
+
+# JWT 토큰 만들기
+jwt = JWTManager(app)
+
+# todo : 로그아웃 개발하고 나서, 코멘트 해제한다.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload) :
+    jti = jwt_payload['jti']
+    return jti in jwt_blacklist
+
+api = Api(app)
+
+# 경로와 리소스를 연결한다.
+api.add_resource(UserRegisterResource, '/api/v1/user/register')
+api.add_resource(UserLoginResource, '/api/v1/user/login')
+api.add_resource(UserLogoutResource, '/api/v1/user/logout')
+api.add_resource(UserInfoResource, '/api/v1/user/me')
+api.add_resource(MovieListResource, '/api/v1/movie')
+api.add_resource(MovieSearchResource, '/api/v1/movie/search')
+api.add_resource(ReviewResource , '/api/v1/review')
+api.add_resource(FavoriteResource, '/api/v1/favorite/<int:movie_id>')
+api.add_resource(FavoriteListResource, '/api/v1/favorite')
+api.add_resource(MovieRealtimeRecommResource, '/api/v1/movie/recommandation')
+
+if __name__ == '__main__' :
+    app.run()
+
